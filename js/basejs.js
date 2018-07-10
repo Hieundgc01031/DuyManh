465ce95b60883a68cebcc29fbb46952475deccfd
Hieu Nguyen <hieundgc01031@fpt.edu.vn>
2018-07-10 08:48:35 +0700

    add index

--- js/basejs.js
+++ js/basejs.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
+    var widthWin = $(window).width();
+    var widtcont = $('.ul-menu .container.absolute').width();
+    var left = (widthWin - widtcont) / 2;
+    $('.ul-menu .container.absolute').css('left', left);
+
 
 });
