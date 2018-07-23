bcd05d8e26279e3e38a1f54faccc0eca468f162a
Hieu Nguyen <hieundgc01031@fpt.edu.vn>
2018-07-23 15:30:55 +0700

    2 page

---
+++ js/tuyensinh.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $('.selectstu--slide').slick({
+        dots:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+
+
+    });
+
+    $('.selectstu__paslide').slick({
+        dots:false,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        arrows:false,
+
+
+    });
+});
+
